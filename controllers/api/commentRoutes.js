2e11beb4a5f78f3a03a1c537aed237e35639a0f1
Elise Harman <elise_harman@hotmail.com>
2021-06-10 11:04:31 +0800

    Fixed typing errors

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,25 +4,25 @@ const { Comment } = require("../../models");
 // View all comments in ascending order by name
 router.get("/", async (request, response) => {
   try {
-    const postData = await Comment.findAll({
+    const commentData = await Comment.findAll({
       order: [["name", "ASC"]],
     });
-    response.status(200).json(postData);
+    response.status(200).json(commentData);
   } catch (error) {
     response.status(500).json(error);
   }
 });
 
-// Add a comments
+// Add a comment
 router.post("/", async (request, response) => {
   // create a new category
   try {
-    const categoryData = await Comment.create({
+    const commentData = await Comment.create({
       content: request.body.content,
       user_id: request.session.user_id,
       post_id: request.body.post_id,
     });
-    response.status(200).json(categoryData);
+    response.status(200).json(commentData);
   } catch (error) {
     response.status(400).json(error);
   }

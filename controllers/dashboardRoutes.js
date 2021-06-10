03750040074cdf75b971e330878e7cd5a3271cfd
Elise Harman <elise_harman@hotmail.com>
2021-06-10 10:56:10 +0800

    Added withAuth

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
 const { Post } = require("../models");
+const withAuth = require("../utils/auth");
 
 // View all posts by a user_id from the current session
-router.get("/", async (request, response) => {
+router.get("/", withAuth, async (request, response) => {
   try {
     const postData = await Post.findAll({
       where: { user_id: request.session.user_id },
     });
-    response.status(200).json(postData);
+
+    const posts = postData.map((project) => project.get({ plain: true }));
+
+    response.render("dashboard", { posts, loggedIn: request.session.loggedIn });
   } catch (error) {
     response.status(500).json(error);
   }
